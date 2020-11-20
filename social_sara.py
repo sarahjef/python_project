f294bc09d7bb4a7f996e0db4aa1941ab48de0a17
sarahjef <jafargholi.id@gmail.com>
2020-11-20 13:05:48 +0330

    compelet def of following people

--- social_sara.py
+++ social_sara.py
@@ -1,132 +1,96 @@
-
-# import numpy as np
 import logging
 import datetime
-import  csv
+import csv
 from hashlib import sha256
 
 
 class Profile:
-    i d =()
-    dat a =[]
-    im g =[]
+    id = ()
+    data = []
+    img = []
 
-
-    def __init__(self ,user ,password ,flag ,email ,phone_no ,status ,comment ,follower ,following):
+    def __init__(self, user, password, flag, email, phone_no, status, comment, follower, following):
 
         logging.basicConfig(filename='app.log', filemode='w', format='%(levelname)s - %(asctime)s - %(message)s',
                             level=logging.INFO)
         logging.info('a profile is created!')
 
-        self.use r =user
-        self.passwor d =password
+        self.user = user
+        self.password = password
 
-        self.fla g =flag
-        self.emai l =email
-        self.phone_n o =phone_no
-        self.statu s =status
-        self.commen t =comment
-        self.followe r =follower
-        self.followin g =following
+        self.flag = flag
+        self.email = email
+        self.phone_no = phone_no
+        self.status = status
+        self.comment = comment
+        self.follower = follower
+        self.following = following
 
+    def save_user_and_pass(self, user, password):
 
-    def save_user_and_pass(self ,user ,password):
-
-        global  data
+        global data
         data = []
         logging.basicConfig(filename='app.log', filemode='w', format='%(levelname)s - %(asctime)s - %(message)s',
                             level=logging.INFO)
         logging.info('user defines his/her user and password and save them!')
 
+        if len(self.user) >= 5 and len(self.password) >= 6:
+            a = sha256(self.user.encode()).hexdigest()
+            b = sha256(self.password.encode()).hexdigest()
 
-
-
-
-
-        if len(self.user )> =5 and len(self.password )> =6: a=sha256(self. user.encode()).hexdigest()
-            b=sha256(self . password.encode()).hexdigest()
             data.append(self.user)
             data.append(b)
 
-            with open('data.csv', 'w', newline='') as csvfile:
-                spamwriter = csv.writer(csvfile, delimiter=' ',
-                                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                spamwriter.writerow(data )
+        return self.user
 
-
-
-
-
-
-        return  self
-
-        .us er,b
-
-
-
-
-
-    def enter_email(se lf,email):
+    def enter_email(self, email):
 
         logging.basicConfig(filename='app.log', filemode='w', format='%(levelname)s - %(asctime)s - %(message)s',
                             level=logging.INFO)
         logging.info('user wants to enter the email!')
 
-
-
-
-
-
-        if '@' in self.email or self.em ai l=="":
-
-
+        if '@' in self.email or self.email == "":
             data.append(self.email)
-            with open('data.csv', 'w', newline='') as csvfile:
-                spamwriter = csv.writer(csvfile, delimiter=' ',
-                                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                spamwriter.writerow(dta )
 
+        return self.email
 
-
-        ret n  self.email
-
-    def phone_nu(se lf,phone_no):
-
-
-
+    def phone_nu(self, phone_no):
 
         logging.basicConfig(filename='app.log', filemode='w', format='%(levelname)s - %(asctime)s - %(message)s',
                             level=logging.INFO)
-        logging.info('user  enters the phone_no!') if  int(self.phone_no):
-            data.append(phone_no)
-            with open('data.csv', 'w', newline='') as csvfile:
-                spamwriter = csv.writer(csvfile, delimiter=' ',
-                                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                spamwriter.writerow(data)
+        logging.info('user  enters the phone_no!')
+
+        data.append(phone_no)
+        with open('data.csv', 'a+', newline='') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter=' ',
+                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            spamwriter.writerow(data)
 
         return phone_no
 
-    def infos_users(self,user) :
+    def infos_users(self, user):
         logging.basicConfig(filename='app.log', filemode='w', format='%(levelname)s - %(asctime)s - %(message)s',
                             level=logging.INFO)
         logging.info('user added to the file and saved!')
-        with open('users.csv', 'w', newline='') as csvfile:
+        with open('users.csv', 'a+', newline='') as csvfile:
             spamwriter = csv.writer(csvfile, delimiter=' ',
                                     quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            spamwriter.writerow("@"+str( s elf.user))
-        return "@"+self . user
+            spamwriter.writerow("@" + str(self.user))
+        return "@" + self.user
+
 
 class Login:
 
-    def __init__(self,enter user,enter pass):
-        self.enteruser=ente r user
-        self.enterpass=ente r pass
-        self.login_status=""
+    def __init__(self, enteruser, enterpass):
+        self.enteruser = enteruser
+        self.enterpass = enterpass
+        self.login_status = ""
 
-    def login1(self,enter user,enter pass):
-        reader=""
-        login_status=""
-        # aa= sha256(self.enteruser.encode()).hexdigest()
+    def login1(self, enteruser, enterpass):
+        logging.basicConfig(filename='app.log', filemode='w', format='%(levelname)s - %(asctime)s - %(message)s',
+                            level=logging.INFO)
+        logging.info('user wants to login to his/her profile!')
+
         bb = sha256(self.enterpass.encode()).hexdigest()
 
         with open('data.csv', newline='') as csvfile:
@@ -134,66 +98,89 @@ class Login:
             spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
 
             for row in spamreader:
+                pass
 
-                cc=
-                print(', '.join(row))
+        self.enteruser
 
-        print(self.enteruser)
-        if row[0] == slf.enteruser :
-            ifrow[1] == bb :
-
-
+        if row[0] == self.enteruser:
+            if row[1] == bb:
 
                 self.login_status = True
                 print("you will able to login")
                 print("hi")
             else:
-                self . login_status=False
+                self.login_status = False
                 print(("you can't log in!!"))
 
-        return self. \
-
-
-login_status
-
+        return self.login_status
 
 
 class Searching_people:
-    d ef __init__(self,user_following):
-        se l f.user_following=user_following
 
-    de f searching(self,user_following):
-        with open('users.csv', newline='') as
+    def __init__(self, user_find):
+        self.user_find = user_find
 
-            csvfile:
+    def searching(self, user_find):
+        logging.basicConfig(filename='app.log', filemode='w', format='%(levelname)s - %(asctime)s - %(message)s',
+                            level=logging.INFO)
+        logging.info('user wants to search for another profile!')
 
+        with open('users.csv', newline='') as csvfile:
 
             spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
 
             for row in spamreader:
-                #
-                #     print(row)
-                if list(self.user_following) == row:
+
+                if list(self.user_find) == row:
                     with open('data.csv', newline='') as csvfile:
 
                         spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
 
                         for line in spamreader:
 
-                            ee=self.user_following.split('@')
-                            if line[0]==ee[1]:
-                                print("the  information o f  profil e  that you s e arche
-                                    d: \ n"+line[0]+" * ******\n"+line[2]+"********\n"+line[3])
-                                pass
+                            ee = self.user_find.split('@')
 
+                            if line[0] == ee[1]:
+                                print("the  information of  profile  that you searched: \n")
+                                for i in range(0, len(line)):
+                                    if i != 1:
+                                        print("   ****************\n" + line[i] + "   ***************\n")
+                                        pass
 
-    def watch_other_profile(self):
-        pass
+    def following(self, user_find):
 
+        no_following = 0
+        f1 = []
+        logging.basicConfig(filename='app.log', filemode='w', format='%(levelname)s - %(asctime)s - %(message)s',
+                            level=logging.INFO)
+        logging.info('user wants to follow another users!')
 
-# Python_project
-import logging
-import datetime
+        with open('data.csv', 'r', newline='') as csvfile:
+
+            lines = csvfile.readlines()
+
+            for m in range(0, len(lines)):
+                lu = lines[m].split()
+                ue = self.user_find.split('@')
+                if ue[1] == lu[0]:
+                    for i in range(0, len(lu)):
+                        if i != 1:
+                            print(lu[i])
+
+        f1.append(self.user_find)
+        no_following += 1
+        return f1, no_following
+
+    def follower(self, follower_list):
+        logging.basicConfig(filename='app.log', filemode='w', format='%(levelname)s - %(asctime)s - %(message)s',
+                            level=logging.INFO)
+        logging.info('user followed by another users!')
+
+        no_follower = 0
+        f2 = []
+        f2.append(self.user_find)
+        no_follower += 1
+        return f2, no_follower
 
 
 # we define class post to use it as an item in our profile_list
@@ -253,6 +240,7 @@ class Post:
 
     # here we check whether the text is in type string or not
     # and we ask user to input a string type
+
     def __str__(self):
         try:
             return "\n" + "The post content: " + "\n" + self.text + "\n" + 8 * "-" + "\n" + "The date:" + str(
@@ -271,53 +259,92 @@ class Post:
 # uncomm
 
 
-rt for trying it out
-
-
-
-
-
-
-
-
-
-
 def main():
-    u = input("enter your personal use r(more than 5 chars):\n")
-    p= input("enter your security password(more than 6 chars):\n")
+    vu = []
+    q = int(input("please enter the number:\n1-you want to login!!!\n 2-you want to make a profil!!\n"))
+    if q == 2:
+        with open('data.csv', 'r', newline='') as csvfile:
 
-    e = input("enter your valid email:\n")
+            lines = csvfile.readlines()
 
-    ph = input( " type your  ph on e_ no: \n ")
+            u = input("enter your personal user(more than 5 chars):\n")
 
-    b=Profile(u,p,0,e,ph,0,0,0,0)
-    print(b.save_user_and_pass(u,p))
-    print(b.enter_email(e))
-    b.phone_nu(ph)
-    prnt(b.infos_users(u))
-    print ("your  pr ofile compel et ed:\n","user=",u,"\ n email=",e ,"\n phone_number=",ph)
+            for m in range(0, len(lines)):
+                lu = lines[m].split()
+                # print(lu[0])
+                vu.append(lu[0])
 
-    eu= input("enter your use r for getting login:\n")
-    ep= input("enter your passwor d  for gett ing login:\n")
+            while u in vu:
+                u = input("please enter another user(more than 5 chars):\n")
+            else:
+                pass
 
-    c=Logi n(eu,ep)
-    print(c.login 1( eu,ep))
-    if c.login_status==True:
-        a = Post("Today is a very good day!")
-        a.comment("your post sucks1!:)")
-        a.comment("your post sucks2!:)")
-        a.comment("your post sucks3!:)")
-        a.comment("your post sucks4!:)")
-        a.edit("hi")
-        print(a)
-        a.delete()
-        print ( a)
-        a.edit(1)
-        g=input("do you want to find your friends? if yo u  want type her id:")
-        h=Searching_people(g)
-        print(h.searching(g))
+            p = input("enter your security password(more than 6 chars):\n")
+
+            e = input("enter your valid email:\n")
+
+            ph = input(" type your  phone_no: \n ")
+
+            b = Profile(u, p, 0, e, ph, 0, 0, 0, 0)
+            b.save_user_and_pass(u, p)
+            b.enter_email(e)
+            b.phone_nu(ph)
+            b.infos_users(u)
+            print("your  profile compeleted:\n" + "    *******************      ", "user=", u, "\n email=", e,
+                  "\n phone_number=", ph + "   *******************     \n")
+            eu = input("enter your user for getting login:\n")
+            ep = input("enter your password  for getting login:\n")
+
+            c = Login(eu, ep)
+            c.login1(eu, ep)
+            if c.login_status == True:
+                a = Post("Today is a very good day!")
+                a.comment("your post sucks1!:)")
+                a.comment("your post sucks2!:)")
+                a.comment("your post sucks3!:)")
+                a.comment("your post sucks4!:)")
+                a.edit("hi")
+                print(a)
+                a.delete()
+                print(a)
+                a.edit(1)
+                g = input("do you want to find your friends? if you  want type her id:")
+                h = Searching_people(g)
+                print(h.searching(g))
+                j = int(input("do you want to follow this profile? 1-yes 2-no\n"))
+                if j == 1:
+                    print("profiles that you followed and the number of following people : \n")
+                    print(h.following(g))
+                if j == 2:
+                    pass
+
+    if q == 1:
+        eu = input("enter your user for getting login:\n")
+        ep = input("enter your password  for getting login:\n")
+
+        c = Login(eu, ep)
+        c.login1(eu, ep)
+        if c.login_status == True:
+
+            a = Post("Today is a very good day!")
+            a.comment("your post sucks1!:)")
+            a.comment("your post sucks2!:)")
+            a.comment("your post sucks3!:)")
+            a.comment("your post sucks4!:)")
+            a.edit("hi")
+            print(a)
+            a.delete()
+            print(a)
+            a.edit(1)
+            g = input("do you want to find your friends? if you  want type her id:")
+            h = Searching_people(g)
+            print(h.searching(g))
+            j = int(input("do you want to follow this profile? 1-yes 2-no\n"))
+            if j == 1:
+                print("profiles that you followed and the number of following people : \n")
+                print(h.following(g))
+            if j == 2:
+                pass
 
 
 main()
-
-
